77b2d7663a6e4cc77afbd72e17a64348b72fa054
Raynard Dodzi Helegah <45526474+dodziraynard@users.noreply.github.com>
2020-11-29 11:49:29 +0000

    update

--- innova_downloader/settings.py
+++ innova_downloader/settings.py
@@ -121,8 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/resources/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'resources/uploads')
